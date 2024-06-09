4de736eda2bb6df1054c125d131aca5ff3b9cc17
LeeJaeHyeok97 <hazardous10@naver.com>
2024-06-09 13:51:12 +0900

    feat: 운동 시간 일별로 쿼리하도록 수정

--- src/main/java/com/mocum/domain/exercise/domain/repository/ExerciseRepository.java
+++ src/main/java/com/mocum/domain/exercise/domain/repository/ExerciseRepository.java
@@ -3,10 +3,15 @@ package com.mocum.domain.exercise.domain.repository;
 import com.mocum.domain.exercise.domain.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     @Query("SELECT SUM(e.exerciseHour) FROM Exercise e WHERE e.user.id = :id")
     Optional<Long> findTotalExerciseHoursByUserId(long id);
+
+    @Query("SELECT SUM(e.exerciseHour) FROM Exercise e WHERE e.user.id = :id AND e.createdAt BETWEEN :startDate AND :endDate")
+    Optional<Long> findDailyExerciseHoursByUserId(@Param("id") long id, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
